12232766c43e75d5a2d7dbf02e96dab464403c6e
Terry <terrybd@gmail.com>
2020-10-27 22:18:38 -0400

    Add consumer for RegisterAccount

---
+++ Customers/customers/Consumers/RegisterAccountConsumer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using commands;
+using MassTransit;
+
+namespace customers.Consumers
+{
+    public class RegisterAccountConsumer : IConsumer<RegisterAccount>
+    {
+        public Task Consume(ConsumeContext<RegisterAccount> context)
+        {
+            Console.WriteLine($"{context.Message.FirstName} {context.Message.Surname} ({context.Message.Email})");
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Customers/customers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using customers.Consumers;
+using MassTransit;
+
+namespace customers
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                cfg.ReceiveEndpoint("register-account", e =>
+                {
+                    e.Consumer<RegisterAccountConsumer>();
+                });
+            });
+
+            var source = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+
+            await busControl.StartAsync(source.Token);
+
+            try
+            {
+                Console.WriteLine("Press enter to exit");
+
+                await Task.Run(Console.ReadLine, source.Token);
+            }
+            finally
+            {
+                await busControl.StopAsync(source.Token);
+            }
+        }
+    }
+}
