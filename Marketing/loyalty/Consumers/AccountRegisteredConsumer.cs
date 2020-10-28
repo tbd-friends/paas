0cbcf546332e81d4ff1d732dceb5d00d407741ae
Terry <terrybd@gmail.com>
2020-10-28 00:00:11 -0400

    Add Marketing loyalty service, consume published event

---
+++ Marketing/loyalty/Consumers/AccountRegisteredConsumer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using events;
+using MassTransit;
+
+namespace loyalty.Consumers
+{
+    public class AccountRegisteredConsumer : IConsumer<AccountRegistered>
+    {
+        public Task Consume(ConsumeContext<AccountRegistered> context)
+        {
+            Console.WriteLine(
+                $"Customer {context.Message.Id} was registered with {context.Message.Surname} ({context.Message.Email})");
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Marketing/loyalty/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using loyalty.Consumers;
+using MassTransit;
+
+namespace loyalty
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                cfg.ReceiveEndpoint("loyalty-service", e =>
+                {
+                    e.Consumer<AccountRegisteredConsumer>();
+                });
+            });
+
+            var source = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+
+            await busControl.StartAsync(source.Token);
+
+            try
+            {
+                Console.WriteLine("Press enter to exit");
+
+                await Task.Run(Console.ReadLine, source.Token);
+            }
+            finally
+            {
+                await busControl.StopAsync(source.Token);
+            }
+        }
+    }
+}
