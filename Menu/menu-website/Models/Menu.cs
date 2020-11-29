237809d139af920d11d4e0299e30b9e5ecd51dd9
Nelson Segarra <junicus@outlook.com>
2020-11-28 23:26:46 -0400

    change ui to have the module be a menu editor, added routes for showing lists of items, categories and menus,

--- Menu/menu-website/Models/Menu.cs
+++ Menu/menu-website/Models/Menu.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections.Generic;
+using Gamer.Menu.Application.Common.Mapper;
 
 namespace Gamer.Menu.Website.Models
 {
-    public class Menu
+    public class Menu : IMapFrom<Core.Models.Menu>
     {
         public string Name { get; set; }
         public IEnumerable<Category> Categories { get; set; }
