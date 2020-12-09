73570008c4a2bfab49a3eddd30e0cde4f0c60363
Terry <terrybd@gmail.com>
2020-12-08 21:41:34 -0500

    Move menu list to a query

---
+++ Menu/menu.viewmodels/MenuCategoryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace menu.viewmodels
+{
+    public class MenuCategoryViewModel
+    {
+        public Guid Uid { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<MenuItemViewModel> Items { get; set; }
+    }
+}
---
+++ Menu/menu.viewmodels/MenuViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace menu.viewmodels
+{
+    public class MenuViewModel
+    {
+        public Guid Uid { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<MenuCategoryViewModel> Categories { get; set; }
+    }
+}
